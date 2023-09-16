835603d2b14fdd8a1d7206ccb2fd04cc84d11c9f
quanmol-wyb <1317954957@qq.com>
2023-09-16 10:46:09 +0800

    change_code

--- new_nk_landscape/test.py
+++ new_nk_landscape/test.py
@@ -2,14 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # 从CSV文件加载数据
-csv_file = "data.csv"  # 你的CSV文件名
+csv_file = "data.csv"  
 df = pd.read_csv(csv_file)
 
 # 提取列数据
-fitness = df['Fitness']  # 替换成你的列名
+fitness = df['Fitness'] 
 
 # 绘制直方图
-plt.figure(figsize=(8, 6))  # 可选的，设置图形大小
+plt.figure(figsize=(8, 6))  
 plt.hist(fitness, bins=100, edgecolor='k', alpha=0.7, label="Fitness")
 
 # 设置图形标题和轴标签
